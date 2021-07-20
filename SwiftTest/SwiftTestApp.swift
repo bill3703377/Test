69f4591d93571bc32c7685ff9e65fd8ff61075fe
hns <hns@hnsdeMacBook-Pro.local>
2021-07-20 21:22:35 +0800

    xxx

---
+++ SwiftTest/SwiftTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestApp.swift
+//  SwiftTest
+//
+//  Created by hns on 2021/7/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
